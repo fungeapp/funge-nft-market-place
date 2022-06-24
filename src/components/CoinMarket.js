66453a41ebec715f17b81f335c72d4f9bb014a50
virlanchainworks <virlanchainworks@gmail.com>
2022-06-24 19:50:55 +0800

    saving datagrid

--- src/components/CoinMarket.js
+++ src/components/CoinMarket.js
@@ -2,11 +2,13 @@ import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
 import { useEffect, useHistory, useState } from 'react';
 import env from 'react-dotenv';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import axios from 'axios'
 
 const CoinMarket = () => {
 
-    const [rank, setrank] = useState();
+    const [id, setrank] = useState();
     const [name, setname] = useState();
+    const [row, setrow] = useState([])
 
 
     const baseURL = `https://api.coinmarketcap.com/data-api/v3/nft/collections?start=0&limit=10&sort=volume&desc=true`;
@@ -15,16 +17,26 @@ const CoinMarket = () => {
         {field:'id', headerName:'Rank', width:70},
         { field: 'name', headerName: 'Name', width: 70 },
     ]
-    const rows = [
+    /*const rows = [
         {id: 1, name:'my collection'},
         {id: 2, name:'my collection'},
         {id: 3, name:'my collection'}
-    ];
+    ];*/
+
+    useEffect(() => {
+        axios.get(baseURL)
+        .then(response => {
+            const result = response.data;
+            console.log(result.data.collections[0].rank);
+            //setrow(`{ id:${} }`)
+
+        })
+    })
 
     return(
         <div style={{ height: 400, width: '100%' }}>
             <DataGrid
-            rows={rows}
+            rows={row}
             columns={columns}
             pageSize={5}
             rowsPerPageOptions={[5]}
