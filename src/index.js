ab53928e284a4509e921ec2fee864b91736b5909
Farhan Ramzan <farhan.ramzan@cache-cloud.com>
2022-06-24 16:25:25 +0500

    login process added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import env from 'react-dotenv';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain={env.AUTH0_DOMAIN}
+    clientId={env.AUTH0_CLIENT_ID}
+    redirectUri={window.location.origin}
+  >
     <App />
+  </Auth0Provider>
   </React.StrictMode>
 );
 
