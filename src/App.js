39d8002ed36800c836a3038df03c06bccf5c174f
Farhan Ramzan <farhan.ramzan@cache-cloud.com>
2022-06-25 18:27:11 +0500

    footer added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react'
 import TopNavBar from './components/TopNavBar';
 import LandingPage from './components/LandingPage';
 import { useAuth0 } from '@auth0/auth0-react';
+import Footer from './components/Footer';
 
 function App() {
   const {loginWithPopup, loginWithRedirect, logout, user, isAuthenticated} = useAuth0();
@@ -12,6 +13,7 @@ function App() {
       <TopNavBar/>
       <h1>{ isAuthenticated ? JSON.stringify(user.given_name, null, 2) : ""}</h1>
       <LandingPage/>
+      <Footer/>
     </div>
   );
 }
