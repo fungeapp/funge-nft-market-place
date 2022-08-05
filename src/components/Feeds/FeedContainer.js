bf2c919d1854333472af9f7b796e6f740f39a105
virlanchainworks <virlanchainworks@gmail.com>
2022-08-05 10:04:59 +0800

    saving for merge

--- src/components/Feeds/FeedContainer.js
+++ src/components/Feeds/FeedContainer.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useHistory, useState } from 'react';
+import env from 'react-dotenv';
+import axios from 'axios';
 import FeedPost from './FeedPost'
 
 const FeedContainer = (props) => {
 
+    const [userid, setuserid] = useState();
+    const [useremail, setuseremail] = useState();
+    
+    useEffect(() => {
+        let id = localStorage.getItem("user_id")
+        let user = localStorage.getItem("user_email")
+        setuserid(id)
+        setuseremail(user)
+        getAllPost();
+    },[userid, useremail]);
+
+    const getAllPost = async(e) => {
+        console.log(`${env.FUNGE_EXPRESSJS_SERVER_BASE_URL}/feeds/post/${userid}`)
+        axios.get(`${env.FUNGE_EXPRESSJS_SERVER_BASE_URL}/feeds/post/${userid}`)
+        /*.then((response) => {
+            console.log(response.data)
+        })*/
+    }
+
     return (
         <>
-            <FeedPost shadow={props.shadow} image="./assets/images/design.png" text="Lorem ipsum dolor sit amet consectetur, adipisicing elit, sed do eiusmod
+            <FeedPost shadow={props.shadow} image="./assets/images/design.png" text="ssssssLorem ipsum dolor sit amet consectetur, adipisicing elit, sed do eiusmod
                     tempor amet, consectetur dipiscing elit,sed do eiusmod tempor"/>
             <FeedPost shadow={props.shadow} image="./assets/images/nft--7.png" postText="Lorem ipsum dolor sit amet consectetur, adipisicing elit, sed do eiusmod
                     tempor amet, consectetur dipiscing elit,sed do eiusmod tempor" />
--- src/components/Feeds/FeedPost.js
+++ src/components/Feeds/FeedPost.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useHistory, useState } from 'react';
 import env from 'react-dotenv';
+import axios from 'axios';
 
 const FeedPost = (props) => {
 
@@ -10,15 +11,6 @@ const FeedPost = (props) => {
 
             </>;
         }
-    }
-
-    useEffect(() => {
-        getAllPost();
-
-    });
-
-    const getAllPost = async => {
-        
     }
 
     const PostImage = (props) => {
@@ -40,25 +32,7 @@ const FeedPost = (props) => {
             <div className="card border-radius my-4 border-0">
                 <div className="card-body">
                     <div className="container-fluid p-0">
-                        <div className="row">
-                            <div className="col-md-12 mt-3 my-2">
-                                <img src="./assets/images/nft-5.png" className="rounded-circle me-3" alt="Cinque Terre" width="30" height="30" />
-                                <span className="fw-bold">Dotdotttt</span>
-                                <span className="float-end "><i className="fa fa-ellipsis-h fw-bold "></i></span>
-                            </div>
-                            <div className="col-md-12 my-2">
-                                {props.text}
-                            </div>
-                        </div>
-                        <PostImage imagePath={props.image} />
-                        <div className="row mb-2 mt-4 px-2">
-                            <div className="col-12 d-flex justify-content-between feedpost-controls">
-                                <i className="fa fa-heart"></i>
-                                <i className="fa fa-commenting">&nbsp; 128</i>
-                                <i className="fa fa-share"></i>
-                                <i className="fa fa-retweet"></i>
-                            </div>
-                        </div>
+                        
                     </div>
                 </div>
             </div>
